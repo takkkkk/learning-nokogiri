5df753b0c012cf1c0299efbc6f8729d514ae3995
takkkkk <git@github.com:takkkkk/sample_app.git>
2016-11-08 02:46:59 +0000

    一行目の値 まで取得

--- debiancode.rb
+++ debiancode.rb
@@ -7,6 +7,17 @@ require 'nokogiri'
 url = 'https://wiki.debian.org/DebianReleases'
 html = open(url).read
 
-puts html
-# doc = Nokogiri::HTML.parse(html)
+#puts html
+doc = Nokogiri::HTML.parse(html)
 # pp doc
+
+tables = doc.xpath('//table')
+strongs = tables[0].xpath('.//strong')
+
+i = 0
+arry = Array.new
+while i < 4 do
+  arry[i] = strongs[i].inner_text
+  print arry[i].to_s.ljust(25)
+  i += 1
+end
