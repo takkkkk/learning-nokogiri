2a4aa0a8aab33ca0dcec0f02b9df25bfb5a11c15
takkkkk <git@github.com:takkkkk/sample_app.git>
2016-11-09 11:49:36 +0000

    Finish this work

--- debiancode.rb
+++ debiancode.rb
@@ -4,27 +4,16 @@ require 'open-uri'
 require 'pp'
 require 'nokogiri'
 
-url = 'https://wiki.debian.org/DebianReleases'
-html = open(url).read
-
-<<<<<<< HEAD
-doc = Nokogiri::HTML.parse(html)
+doc = Nokogiri::HTML(open('https://wiki.debian.org/DebianReleases'))
 
 tables = doc.xpath('//table')
 
-=======
-#puts html
-doc = Nokogiri::HTML.parse(html)
-# pp doc
-
-tables = doc.xpath('//table')
->>>>>>> 5df753b0c012cf1c0299efbc6f8729d514ae3995
-strongs = tables[0].xpath('.//strong')
-
-i = 0
-arry = Array.new
-while i < 4 do
-  arry[i] = strongs[i].inner_text
-  print arry[i].to_s.ljust(25)
-  i += 1
+# テーブル2つ分取得
+tables.first.xpath('.//tr').each do |tr| # テーブルから行を取得
+  tr.xpath('.//td').each do |td| # 取得した各行に対して、tdタグを指定し、その要素をljustメソッドによって左寄せにする。
+    print td.inner_text.ljust(30)
+  end
+  puts
 end
+
+
